f8e5ff42e4e144ab4734d829ce925eb50b5db6be
murzaevas <nastia.murzaeva@yandex.ru>
2019-01-16 09:27:30 +0400

    добавлен список

--- SnakeGame/SnakeGame/Program.cs
+++ SnakeGame/SnakeGame/Program.cs
@@ -11,31 +11,34 @@ namespace SnakeGame
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            /*p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';*/
+         
             p1.Draw();
-
-            //Draw(p1.x, p1.y, p1.sym);
-            /*int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);*/
-
-            Point p2 = new Point(4,5,'#');
-            /*p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';*/
+                    
+           Point p2 = new Point(4,5,'#');
+           
             p2.Draw();
 
-            //Draw(p2.x, p2.y, p2.sym);
-            /*int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);*/
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
 
-            Console.ReadLine();
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+           Console.ReadLine();
         }
 
     }
